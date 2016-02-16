ee68334e6c372e49c97af1272e6a67b65d9946d3
JohanJansson62 <janssjo@3se-spel-19.utb.fuv.se>
2016-02-16 13:39:37 +0100

    Allt banor Johan Jansson

---
+++ johan/Assets/myScripts/Car/CarUserControl.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+    [RequireComponent(typeof (CarController))]
+    public class CarUserControl : MonoBehaviour
+    {
+        [SerializeField] private int m_playerInput;
+        private CarController m_Car; // the car controller we want to use
+
+
+        private void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+        }
+
+
+        private void FixedUpdate()
+        {
+            // pass the input to the car!
+
+            if (m_playerInput == 1)
+            {
+                float h = CrossPlatformInputManager.GetAxis("PlayerOneH");
+                float v = CrossPlatformInputManager.GetAxis("PlayerOneV");
+
+                float handbrake = CrossPlatformInputManager.GetAxis("PlayerOneBrake");
+                m_Car.Move(h, v, v, handbrake);
+            }
+
+            if(m_playerInput == 2)
+            {
+                float h = CrossPlatformInputManager.GetAxis("PlayerTwoH");
+                float v = CrossPlatformInputManager.GetAxis("PlayerTwoV");
+
+                float handbrake = CrossPlatformInputManager.GetAxis("PlayerTwoBrake");
+                m_Car.Move(h, v, v, handbrake);
+            }
+
+            if(m_playerInput == 3)
+            {
+                float h = CrossPlatformInputManager.GetAxis("PlayerThreeH");
+                float v = CrossPlatformInputManager.GetAxis("PlayerThreeV");
+
+                float handbrake = CrossPlatformInputManager.GetAxis("PlayerThreeBrake");
+                m_Car.Move(h, v, v, handbrake);
+            }
+
+            if(m_playerInput == 4)
+            {
+                float h = CrossPlatformInputManager.GetAxis("PlayerFourH");
+                float v = CrossPlatformInputManager.GetAxis("PlayerFourV");
+
+                float handbrake = CrossPlatformInputManager.GetAxis("PlayerFourBrake");
+                m_Car.Move(h, v, v, handbrake);
+            }
+
+        }
+    }
+
---
+++ johan/Assets/myScripts/WorldObject/GameTimer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class GameTimer : MonoBehaviour
+{
+    [SerializeField] private float m_timeCount = 60f; // Initialize the start time for each round in gamemode Crown. This serialized variable is mincapped @ 60 seconds.
+    private float m_maxTimeCount;
+    private bool m_playerIsReady = false;
+
+    IEnumerator StartDelay()
+    {
+        yield return new WaitForSeconds(3);
+        m_playerIsReady = false;
+    }
+
+    void Awake()
+    {
+        StartDelay(); //Before game start, check if players are ready - tick down 3 seconds before game start.
+    }
+
+    void Start ()
+    {
+        m_maxTimeCount = m_timeCount;
+	}
+	
+	
+	void FixedUpdate ()
+    {
+        m_timeCount -= Time.deltaTime; //Counts down time per seconds per frame
+
+        if(m_timeCount <= 0f)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+            
+        }
+	}
+}
